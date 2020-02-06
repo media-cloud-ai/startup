1b35b387cfdc052188edd7907fa0754253fc562e
Stéphane Colalongo <scolalongo@gmail.com>
2020-02-06 18:07:13 +0100

    Reidrect output and error to /dev/null to avoid unwanted logs.

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
 if [ $# -eq 1 ]; then
-    service=$1;
+    service=$1
 else
     exit 1
 fi
 
 hostname="$(printenv ${service}_HOSTNAME)"
 port="$(printenv ${service}_PORT)"
-ping -q -c 1 ${hostname}
+ping -q -c1 ${hostname} > /dev/null 2>/dev/null
 
 exit $?
